b348a70ff20dca09f016d81140f3b6fc404d88c4
Dan Norman <normande@pkcsecurity.com>
2018-02-09 22:38:57 +0200

    more mapping

--- src/parable/models/mapper.clj
+++ src/parable/models/mapper.clj
@@ -12,17 +12,32 @@
 
 (defn link [{:keys [body]}]
   (first (keep identity
-           (for [[priv partners] @private->partners] 
-             (when-let [partner (first partners)]
-               (reset! private->partners (update @private->partners priv disj partner))
-               (swap! seeker->partners conj {:seekerNumber body
-                                             :partnerNumber partner})
-               {priv partner}))))) 
+               (for [[priv partners] @private->partners] 
+                 (when-let [partner (first partners)]
+                   (reset! private->partners (update @private->partners priv disj partner))
+                   ;TODO: need to rm it from the map too
+                   ; test this:
+                   (reset! seeker->partners
+                           (remove @seeker->partners #(and 
+                                                        (= (get-in % :seekerNumber) body)
+                                                        (= (get-in % :partnerNumber) partner))))
+                   (swap! seeker->partners conj {:seekerNumber body
+                                                 :partnerNumber partner})
+                   {priv partner}))))) 
 
+;provide a partner
+;get a new seeker->private->partner mapping
 (defn link-partner [{:keys [body]}]
   (for [[priv partners] @private->partners]
     (when (get @partners (:partnerNumber body))
-      (swap! private->partners update @private->partners priv disj (:partnerNumber body))
+      (reset! private->partners (update @private->partners priv disj (:partnerNumber body)))
+     ;TODO: this is wrong, fix it for this function:
+     (reset! seeker->partners
+              (remove @seeker->partners #(and 
+                                           (= (get-in % :seekerNumber) body)
+                                           (= (get-in % :partnerNumber) partner))))     
+      (swap! seeker->partners conj {:seekerNumber (:seekerNumber body)
+                                                                                                                      :partnerNumber (:partnerNumber body)})
       priv)))
 
 (defn available-partners []
